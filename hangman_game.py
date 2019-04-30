2a892196de2dad99bd1556ba1576fd814fe8eb74
Leung <jonathan.k.leung@accenturefederal.com>
2019-04-30 10:13:20 -0400

    argparse functionality added

--- hangman_game.py
+++ hangman_game.py
@@ -1,13 +1,19 @@
+import argparse
+parser = argparse.ArgumentParser(description='Input a minimum word length for hangman')  # create object
+parser.add_argument("-l", '--minLength', type=int, required=True,
+                    help='Minimum word length for hangman target word \n'
+                         'largest value is 15 characters')  # add minLength argument
+args = parser.parse_args()  # parse the arguments
+
 
 class Hangman():
     def __init__(self):
-        self.gameState = 1  # initialize state of game, 1 means on going, 0 means GAME OVER
         self.hangmanState = 6  # used to inform hangman display
         self.guessedLetters = []  # list to store guessed letters
         self.wordList = []  # list of hangman target words
         self.url = 'http://norvig.com/ngrams/sowpods.txt'  # url to for text file
         import string
-        self.validLetters = list(string.ascii_uppercase)
+        self.validLetters = list(string.ascii_uppercase)  # use uppercase letters for game
 
     def checkFile (self):
         # returns the filepath of the sowpods.txt file
@@ -47,77 +53,76 @@ class Hangman():
         target = random.choice(wordList)  # select a random value
         return target
 
-    def setupGame(self):
-        gameReady = False
-        while not gameReady:
-            minLength = input('Choose a minimum word length ')
-            try:
-                minLength = int(minLength)  # convert value into integer
-            except ValueError:
-                print('Please choose an integer')
-            else:
-                gameReady = True  # no errors? then the game is ready!
+    def setupGame(self, minLength):
+        # gameReady = False
+        # while not gameReady:
+        #     try:
+        #         minLength = int(minLength)  # ensure
+        #     except ValueError:
+        #         print('Please choose an integer')
+        #     else:
+        #         gameReady = True  # no errors? then the game is ready!
         wordList = self.trimList(minLength)  # create a wordList using the minLength
         target = self.pickTarget(wordList)
         return target
 
     def showBoard(self):
         if self.hangmanState == 6:
-            print('  |‾‾‾‾‾‾‾|')
+            print('  |‾‾‾‾|')
             print('  |      ')
             print('  |      ')
             print('  |      ')
             print('__|__\n')
         if self.hangmanState == 5:
-            print('  |‾‾‾‾‾‾‾|')
-            print('  |     0')
+            print('  |‾‾‾‾|')
+            print('  |    0')
             print('  |      ')
             print('  |      ')
             print('__|__\n')
         if self.hangmanState == 4:
-            print('  |‾‾‾‾‾‾‾|')
-            print('  |     0')
-            print('  |    / ')
+            print('  |‾‾‾‾|')
+            print('  |    0')
+            print('  |   / ')
             print('  |      ')
             print('__|__\n')
         if self.hangmanState == 3:
-            print('  |‾‾‾‾‾‾‾|')
-            print('  |     0')
-            print('  |    /| ')
+            print('  |‾‾‾‾|')
+            print('  |    0')
+            print('  |   /| ')
             print('  |      ')
             print('__|__\n')
         if self.hangmanState == 2:
-            print('  |‾‾‾‾‾‾‾|')
-            print('  |     0')
-            print('  |    /|\ ')
+            print('  |‾‾‾‾|')
+            print('  |    0')
+            print('  |   /|\ ')
             print('  |      ')
             print('__|__\n')
         if self.hangmanState == 1:
-            print('  |‾‾‾‾‾‾‾|')
-            print('  |     0')
-            print("  |    /|\ ")
-            print('  |    /' )
+            print('  |‾‾‾‾|')
+            print('  |    0')
+            print("  |   /|\ ")
+            print('  |   / ' )
             print('__|__\n')
         if self.hangmanState <= 0:
-            print('  |‾‾‾‾‾‾‾|')
-            print('  |     0')
-            print("  |    /|\ ")
-            print("  |    / \ ")
+            print('  |‾‾‾‾|')
+            print('  |    0')
+            print("  |   /|\ ")
+            print("  |   / \ ")
             print('__|__\n')
 
     def showSplit(self, array):
         print(' '.join(array))  # format output for hangman
 
-    def playGame(self):
+    def playGame(self, minLength):
         import numpy as np
-        target = self.setupGame()  # specify target word with setupGame
+        target = self.setupGame(minLength)  # specify target word with setupGame
         targetSplit = np.array(list(target))  # created for finding correct guesses
         maskedSplit = np.array(['_' for letter in list(target)])  # created to show state of hangman game
 
         while self.hangmanState > 0:
             self.showBoard()  # start off by showing state of hangman
             self.showSplit(maskedSplit)  # display state of board
-            print(target)  # used for debugging
+            # print(target)  # used for debugging
             letterGuess = input('Guess a letter ')
             if letterGuess.upper() in self.validLetters:
                 if letterGuess.upper() in self.guessedLetters:
@@ -144,8 +149,13 @@ class Hangman():
             print('Game Over!')
             self.showSplit(maskedSplit)  # show the masked split
             self.showBoard()  #  show the board
-            print('Correct word was {}'.format(target))  # tell user what correct word was 
+            print('Correct word was {}'.format(target))  # tell user what correct word was
 
 
-mygame = Hangman()
-mygame.playGame()
+
+def main():
+    game = Hangman()  # instantiate the class
+    game.playGame(args.minLength)  # play the game using the argument from argparse
+
+if __name__ == '__main__':
+    main()  # call the main function
