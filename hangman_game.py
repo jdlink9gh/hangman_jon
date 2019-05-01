f072e146e6839a9367a3dd434b89364dddd05e87
Leung <jonathan.k.leung@accenturefederal.com>
2019-05-01 11:25:25 -0400

    Updated showBoard method

--- hangman_game.py
+++ hangman_game.py
@@ -1,10 +1,12 @@
-import argparse
-parser = argparse.ArgumentParser(description='Input a minimum word length for hangman')  # create object
-parser.add_argument("-l", '--minLength', type=int, required=True,
-                    help='Minimum word length for hangman target word \n'
-                         'largest value is 15 characters')  # add minLength argument
-args = parser.parse_args()  # parse the arguments
 
+# import packages
+import argparse
+import numpy as np
+import os
+import random
+import string
+
+# create hangman class
 
 class Hangman():
     def __init__(self):
@@ -12,13 +14,13 @@ class Hangman():
         self.guessedLetters = []  # list to store guessed letters
         self.wordList = []  # list of hangman target words
         self.url = 'http://norvig.com/ngrams/sowpods.txt'  # url to for text file
-        import string
         self.validLetters = list(string.ascii_uppercase)  # use uppercase letters for game
+        self.minLength = 1  # cutoff word length for hangman game
 
     def checkFile (self):
         # returns the filepath of the sowpods.txt file
         # create a folder called word_data and save the file as words.txt
-        import os
+
         fileExists = os.path.isfile('./word_data/words.txt')  # boolean to see if file exits
         if fileExists:
             return os.path.relpath('./word_data/words.txt')  # return the path of the file
@@ -33,89 +35,47 @@ class Hangman():
                 textFile = urllib.request.urlretrieve(self.url, './word_data/words.txt')  # download the file
                 return os.path.relpath(textFile[0])  # return the path
 
-    def openFile(self):
-        # opens the text file, returns raw list of words
-        filepath = self.checkFile()  # call check file, which returns the relative file pile
-        with open(filepath) as f:
-            wordList = f.readlines()  # use readlines to create list of words
-            wordList = [word.strip('\n') for word in wordList]  # take out whitespace characters
-        return wordList  # return list of words
-
-    def trimList(self, minLength):
-        # minLength represents minimum string length
-        self.wordList = self.openFile()  # populate wordList with openFile which
-        self.wordList = [word for word in self.wordList if len(word) >= minLength]
-        return self.wordList
-
-    def pickTarget(self, wordList):
-        #  select a random value from a list
-        import random
-        target = random.choice(wordList)  # select a random value
-        return target
-
-    def setupGame(self, minLength):
-        # gameReady = False
-        # while not gameReady:
-        #     try:
-        #         minLength = int(minLength)  # ensure
-        #     except ValueError:
-        #         print('Please choose an integer')
-        #     else:
-        #         gameReady = True  # no errors? then the game is ready!
-        wordList = self.trimList(minLength)  # create a wordList using the minLength
-        target = self.pickTarget(wordList)
+    def setupGame(self):
+        filepath = self.checkFile()  # check the file to see if it exists, if not create it
+        with open(filepath) as f:  # open the file and populate self.wordList
+            self.wordList = [word.strip('\n') for word in f.readlines()]  # take out whitespace characters
+        self.wordList = [word for word in self.wordList if len(word) >= self.minLength]  # trim using self.minLength
+        target = random.choice(self.wordList)  # randomly pick a word from self.wordList
         return target
 
     def showBoard(self):
-        if self.hangmanState == 6:
-            print('  |‾‾‾‾|')
-            print('  |      ')
-            print('  |      ')
-            print('  |      ')
-            print('__|__\n')
-        if self.hangmanState == 5:
-            print('  |‾‾‾‾|')
-            print('  |    0')
-            print('  |      ')
-            print('  |      ')
-            print('__|__\n')
-        if self.hangmanState == 4:
-            print('  |‾‾‾‾|')
-            print('  |    0')
-            print('  |   / ')
-            print('  |      ')
-            print('__|__\n')
-        if self.hangmanState == 3:
-            print('  |‾‾‾‾|')
-            print('  |    0')
-            print('  |   /| ')
-            print('  |      ')
-            print('__|__\n')
-        if self.hangmanState == 2:
-            print('  |‾‾‾‾|')
-            print('  |    0')
-            print('  |   /|\ ')
-            print('  |      ')
-            print('__|__\n')
-        if self.hangmanState == 1:
-            print('  |‾‾‾‾|')
-            print('  |    0')
-            print("  |   /|\ ")
-            print('  |   / ' )
-            print('__|__\n')
-        if self.hangmanState <= 0:
-            print('  |‾‾‾‾|')
-            print('  |    0')
-            print("  |   /|\ ")
-            print("  |   / \ ")
-            print('__|__\n')
+        print('{} remaining guesses'.format(self.hangmanState))
+        print('  |‾‾‾‾|')  # print line 1 of 5
+        if self.hangmanState <= 5:
+            print('  |    0')  # print line 2 of 5
+            if 2 <= self.hangmanState <= 4:
+                if self.hangmanState == 4:
+                    print('  |   / ')
+                if self.hangmanState == 3:
+                    print('  |   /| ')
+                if self.hangmanState == 2:
+                    print('  |   /|\ ')
+                print('  |      ')
+            if 0 <= self.hangmanState <= 1:
+                print('  |   /|\ ')
+                if self.hangmanState == 1:
+                    print('  |   / ')
+                if self.hangmanState <= 0:
+                    print('  |   / \ ')
+            if self.hangmanState == 5:
+                print('  |    ')
+                print('  |    ')
+        else:
+            for i in range(3):
+                print('  |      ')
+        print('__|__\n')  
+
 
     def showSplit(self, array):
         print(' '.join(array))  # format output for hangman
 
-    def playGame(self, minLength):
-        import numpy as np
-        target = self.setupGame(minLength)  # specify target word with setupGame
+    def playGame(self):
+        target = self.setupGame()  # specify target word with setupGame
         targetSplit = np.array(list(target))  # created for finding correct guesses
         maskedSplit = np.array(['_' for letter in list(target)])  # created to show state of hangman game
 
@@ -138,7 +98,7 @@ class Hangman():
                             print('You won the game!')
                             self.showSplit(maskedSplit)  # show the completed word
                             self.showBoard()
-                            break  # exit the game since word was correctly guessed
+                            self.hangmanState = -1  # exit the game since word was correctly guessed
                     else:
                         self.hangmanState -= 1  # decrement hangman state for incorrect guess
                         print('Incorrect guess')
@@ -154,8 +114,15 @@ class Hangman():
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Input a minimum word length for hangman')  # create object
+    parser.add_argument("-l", '--minLength', type=int, required=True,
+                        help='Minimum word length for hangman target word \n'
+                             'largest value is 15 characters')  # add minLength argument
+    args = parser.parse_args()  # parse the arguments
+
     game = Hangman()  # instantiate the class
-    game.playGame(args.minLength)  # play the game using the argument from argparse
+    game.minLength = args.minLength  # set the minimum length object equal to the argument
+    game.playGame()  # play the game
 
 if __name__ == '__main__':
     main()  # call the main function
